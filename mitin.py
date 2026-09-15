5ccb11c93af208beaeb670cbc7b08e8aa4c0baed
Yash Chiney <yrchiney@gmail.com>
2026-09-15 19:28:40 +0530

    Update mitin.py

--- mitin.py
+++ mitin.py
@@ -14,9 +14,9 @@ with col3:
 
 with col4:
   b4=st.button('Signup as :red[student]')
-st.title('What is :rainbow[MITin]')
+st.title('What is :rainbow[MITin]?')
 st.write('MITin is an exclusive digital platform designed for MIT-WPU students and recruiters to connect through student projects, skills, achievements, and academic profiles. It provides students with a single place to showcase their projects, certifications, technical skills, CGPA, coding languages, and other achievements, while allowing recruiters to discover suitable candidates without having to manually search through multiple sources.') 
-st.title('Why use :rainbow[MITin]')
+st.title('Why use :rainbow[MITin]?')
 st.markdown(
   """
   Student projects and achievements are often scattered across different platforms, making it difficult for recruiters to identify the right students. MITin solves this by bringing everything together in one college-specific platform.
