7dbcdd4fb2329a6b312f123cf53af05abd07008f
Yash Chiney <yrchiney@gmail.com>
2026-09-15 17:18:46 +0530

    Initialize MITin app with Streamlit interface

---
+++ mitin.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.write('# Welcome to MITin ')
+st.write('Your talent finding platform')
+st.write('# Choose your type of access')
+st.button('Login as recruiter')
+st.button('Login as student')
+st.button('Signup as recruiter')
+st.button('Signup as student')
