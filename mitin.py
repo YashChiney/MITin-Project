786cba4f53a371ce8e296ab42a3ebe1c8092525b
Yash Chiney <yrchiney@gmail.com>
2026-09-15 18:05:04 +0530

    Refactor button layout into columns

--- mitin.py
+++ mitin.py
@@ -2,7 +2,15 @@ import streamlit as st
 st.title(' Welcome to :rainbow[MITin] ')
 st.write(':blue[Your talent finding platform]')
 st.write('# Choose your type of access')
-st.button('Login as :blue[recruiter]')
-st.button('Login as :green[student]')
-st.button('Signup as :violet[recruiter]')
-st.button('Signup as :red[student]')
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+  b1=st.button('Login as :blue[recruiter]')
+
+with col2:
+  b2=st.button('Login as :green[student]')
+
+with col3:
+  b3=st.button('Signup as :violet[recruiter]')
+
+with col4:
+  b4=st.button('Signup as :red[student]')
